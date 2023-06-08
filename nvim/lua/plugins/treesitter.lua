771458eb2f1f8d2151afd042b7f48cee3de2da28
moecasts <moecasts.caster@gmail.com>
2023-06-08 19:22:51 +0800

    feat: optimize treesitter highlight

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -13,7 +13,17 @@ return {
     },
     ---@type TSConfig
     opts = {
-      highlight = { enable = true },
+      highlight = {
+        enable = true,
+        -- disable slow treesitter highlight for large files
+        disable = function(lang, buf)
+          local max_filesize = 10 * 1024 -- 10 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+      },
       indent = { enable = true },
       context_commentstring = { enable = true, enable_autocmd = false },
       ensure_installed = {
