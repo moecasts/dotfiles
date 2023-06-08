6e138f4d9fde635265a16c5ab5fa1f89964dcbd8
moecasts <moecasts.caster@gmail.com>
2023-06-08 19:22:51 +0800

    feat(nvim): add nvim-spectre plugin

---
+++ nvim/lua/plugins/spectre.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'nvim-pack/nvim-spectre',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    config = function()
+      require('spectre').setup({})
+    end,
+  },
+}
