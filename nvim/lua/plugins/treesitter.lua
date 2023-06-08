5d0463869168c7df6130587ff730fcf94d9a1f96
moecasts <moecasts.caster@gmail.com>
2023-06-08 19:22:51 +0800

    feat(nvim): add fold plugin

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -51,4 +51,29 @@ return {
       require('nvim-treesitter.configs').setup(opts)
     end,
   },
+
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'kevinhwang91/promise-async',
+    },
+    config = function()
+      require('ufo').setup({
+        provider_selector = function(bufnr, filetype, buftype)
+          return { 'treesitter', 'indent' }
+        end,
+      })
+      vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+      vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
+      vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+      vim.keymap.set('n', 'K', function()
+        local winid = require('ufo').peekFoldedLinesUnderCursor()
+        if not winid then
+          vim.lsp.buf.hover()
+        end
+      end)
+    end,
+  },
 }
