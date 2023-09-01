58a8a7aa0a695d5c9ec6fed7af878a674064425f
Caster <moecasts.caster@gmail.com>
2023-09-01 11:58:48 +0800

    fix(nvim): ts context comment

--- nvim/lua/plugins/coding/init.lua
+++ nvim/lua/plugins/coding/init.lua
@@ -120,16 +120,14 @@ return {
     'echasnovski/mini.comment',
     event = 'VeryLazy',
     opts = {
-      hooks = {
-        pre = function()
-          require('ts_context_commentstring.internal').update_commentstring({})
+      options = {
+        custom_commentstring = function()
+          return require('ts_context_commentstring.internal').calculate_commentstring() or vim.bo.commentstring
         end,
       },
     },
-    config = function(_, opts)
-      require('mini.comment').setup(opts)
-    end,
   },
+
   {
     'kkoomen/vim-doge',
     build = function(plugin)
