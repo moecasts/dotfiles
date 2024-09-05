7211342dae3d239b7f6b80a7cc6ac70c2dbdf62d
moecasts <moecasts.caster@gmail.com>
2024-09-05 17:35:01 +0800

    feat(nvim): add leetcode plugin

---
+++ nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    'kawre/leetcode.nvim',
+    build = ':TSUpdate html',
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+      'nvim-lua/plenary.nvim', -- required by telescope
+      'MunifTanjim/nui.nvim',
+
+      -- optional
+      'nvim-treesitter/nvim-treesitter',
+      'rcarriga/nvim-notify',
+      'nvim-tree/nvim-web-devicons',
+    },
+    opts = {
+      ---@type lc.lang
+      lang = 'typescript',
+
+      -- configuration goes here
+      plugins = {
+        non_standalone = true,
+      },
+    },
+  },
+}
