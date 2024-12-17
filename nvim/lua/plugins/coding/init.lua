f0ba49eb0efddc78b8d10d1504e1fc51f30579e1
Caster <moecasts.caster@gmail.com>
2024-12-17 22:17:40 +0800

    style(nvim): prettier code

--- nvim/lua/plugins/coding/init.lua
+++ nvim/lua/plugins/coding/init.lua
@@ -22,10 +22,12 @@ return {
         function()
           return require('luasnip').jumpable(1) and '<Plug>luasnip-jump-next' or '<tab>'
         end,
-        expr = true, silent = true, mode = 'i',
+        expr = true,
+        silent = true,
+        mode = 'i',
       },
-      { '<tab>', function() require('luasnip').jump(1) end, mode = 's' },
-      { '<s-tab>', function() require('luasnip').jump( -1) end, mode = { 'i', 's' } },
+      { '<tab>',   function() require('luasnip').jump(1) end,   mode = 's' },
+      { '<s-tab>', function() require('luasnip').jump(-1) end,  mode = { 'i', 's' } },
     },
   },
 
