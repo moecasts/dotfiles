d599c83fcb2a76ec73850dcfb6f0dfc165195e9e
moecasts <moecasts.caster@gmail.com>
2025-01-24 10:40:30 +0800

    feat(nvim): disabled neotest keys

--- nvim/lua/plugins/extras/test/init.lua
+++ nvim/lua/plugins/extras/test/init.lua
@@ -107,7 +107,7 @@ return {
       { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File (Neotest)" },
       { "<leader>tT", function() require("neotest").run.run(vim.uv.cwd()) end, desc = "Run All Test Files (Neotest)" },
       { "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest (Neotest)" },
-      { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last (Neotest)" },
+      -- { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last (Neotest)" },
       { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary (Neotest)" },
       { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output (Neotest)" },
       { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel (Neotest)" },
