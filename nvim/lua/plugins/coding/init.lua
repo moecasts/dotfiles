fcbdb1e71f2c8eb501596e50bc881f557d597a6c
Caster <moecasts.caster@gmail.com>
2025-02-06 02:11:31 +0800

    feat(nvim): use deepseek-reasoner model

--- nvim/lua/plugins/coding/init.lua
+++ nvim/lua/plugins/coding/init.lua
@@ -228,7 +228,8 @@ return {
       auto_suggestions_provider = 'openai', -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
       openai = {
         endpoint = 'https://api.deepseek.com/v1',
-        model = 'deepseek-chat',
+        -- model = 'deepseek-chat',
+        model = 'deepseek-reasoner',
         timeout = 30000, -- Timeout in milliseconds
         temperature = 0,
         max_tokens = 4096,
