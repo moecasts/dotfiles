3bba71b2b3619832fadacdda1f107bebbeac453c
moecasts <moecasts.caster@gmail.com>
2025-03-06 20:45:28 +0800

    feat(nvim): add neotree key for avante add files

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -158,6 +158,28 @@ return {
                 grug_far.update_instance_prefills('explorer', prefills, false)
               end
             end,
+
+            avante_add_files = function(state)
+              local node = state.tree:get_node()
+              local filepath = node:get_id()
+              local relative_path = require('avante.utils').relative_path(filepath)
+
+              local sidebar = require('avante').get()
+
+              local open = sidebar:is_open()
+              -- ensure avante sidebar is open
+              if not open then
+                require('avante.api').ask()
+                sidebar = require('avante').get()
+              end
+
+              sidebar.file_selector:add_selected_file(relative_path)
+
+              -- remove neo tree buffer
+              if not open then
+                sidebar.file_selector:remove_selected_file('neo-tree filesystem [1]')
+              end
+            end,
           },
           window = {
             mappings = {
@@ -165,6 +187,7 @@ return {
               ['Y'] = 'copy_selector',
               ['F'] = 'find_in_directory_grug_far',
               ['T'] = 'find_in_directory_telescope',
+              ['oa'] = 'avante_add_files',
             },
           },
         },
