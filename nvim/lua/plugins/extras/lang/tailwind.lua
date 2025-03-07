88d477811950dcfaaee1b1ab6de4f2b8ce4fb21f
moecasts <moecasts.caster@gmail.com>
2025-03-08 03:03:32 +0800

    feat(nvim): add tailwind support

---
+++ nvim/lua/plugins/extras/lang/tailwind.lua
@@ -0,0 +1,74 @@
+local config_files = {
+  root = {
+    'tailwind.config.js',
+    'tailwind.config.cjs',
+    'tailwind.config.mjs',
+    'tailwind.config.ts',
+    'postcss.config.js',
+    'postcss.config.cjs',
+    'postcss.config.mjs',
+    'postcss.config.ts',
+  },
+}
+
+return {
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        tailwindcss = {
+          -- exclude a filetype from the default_config
+          filetypes_exclude = { 'markdown' },
+          -- add additional filetypes to the default_config
+          filetypes_include = {},
+          -- to fully override the default_config, change the below
+          -- filetypes = {}
+        },
+      },
+      setup = {
+        tailwindcss = function(_, opts)
+          local tw = Editor.lsp.get_raw_config('tailwindcss')
+          opts.filetypes = opts.filetypes or {}
+
+          -- Add default filetypes
+          vim.list_extend(opts.filetypes, tw.default_config.filetypes)
+
+          -- Remove excluded filetypes
+          --- @param ft string
+          opts.filetypes = vim.tbl_filter(function(ft)
+            return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+          end, opts.filetypes)
+
+          -- Additional settings for Phoenix projects
+          opts.settings = {
+            tailwindCSS = {
+              includeLanguages = {
+                elixir = 'html-eex',
+                eelixir = 'html-eex',
+                heex = 'html-eex',
+              },
+            },
+          }
+
+          -- Add additional filetypes
+          vim.list_extend(opts.filetypes, opts.filetypes_include or {})
+        end,
+      },
+    },
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    optional = true,
+    dependencies = {
+      { 'roobert/tailwindcss-colorizer-cmp.nvim', opts = {} },
+    },
+    opts = function(_, opts)
+      -- original LazyVim kind icon formatter
+      local format_kinds = opts.formatting.format
+      opts.formatting.format = function(entry, item)
+        format_kinds(entry, item) -- add icons
+        return require('tailwindcss-colorizer-cmp').formatter(entry, item)
+      end
+    end,
+  },
+}
