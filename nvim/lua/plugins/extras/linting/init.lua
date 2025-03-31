7c490ed7109df9dca403d4bebf8b84c20e504f21
moecasts <moecasts.caster@gmail.com>
2025-03-31 20:27:43 +0800

    feat(nvim): add LintInfo command

--- nvim/lua/plugins/extras/linting/init.lua
+++ nvim/lua/plugins/extras/linting/init.lua
@@ -99,6 +99,18 @@ return {
         group = vim.api.nvim_create_augroup('nvim-lint', { clear = true }),
         callback = M.debounce(100, M.lint),
       })
+
+      -- Show linters for the current buffer's file type
+      vim.api.nvim_create_user_command('LintInfo', function()
+        local filetype = vim.bo.filetype
+        local linters = require('lint').linters_by_ft[filetype]
+
+        if linters then
+          print('Linters for ' .. filetype .. ': ' .. table.concat(linters, ', '))
+        else
+          print('No linters configured for filetype: ' .. filetype)
+        end
+      end, {})
     end,
   },
 }
